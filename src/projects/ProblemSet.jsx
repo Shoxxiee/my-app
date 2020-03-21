09d456cda88cf155a38f0e82bc4b3cf7a6627a58
Missula Kartik <kartik96brainstorm@outlook.com>
2020-03-22 00:33:37 +0530

    Updated code with fixtures

---
+++ src/projects/ProblemSet.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { data } from "./fixtures";
+
+class ProblemSet extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ProblemSet;
